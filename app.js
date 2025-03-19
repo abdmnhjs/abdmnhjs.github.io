6be0ab584c7ef3e95ec045a15c88b2a1a77dc808
abd <22403728@me.uvsq.fr>
2025-03-19 10:21:06 +0100

    qfqd

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll('.skill, .project');
+    const elements = document.querySelectorAll('.skill, .project, .etudes-description li');
 
-    let delay = 150;
-    elements.forEach((element, index) => {
-        setTimeout(() => {
-            element.style.opacity = 1;
-            element.style.transform = 'translateY(0)';
-        }, delay);
-        delay += 150;
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+                entry.target.style.transform = 'translateY(0)';
+                observer.unobserve(entry.target); // Arrêter d'observer l'élément une fois qu'il est animé
+            }
+        });
+    }, {
+        threshold: 0.1 // Déclencher l'animation lorsque 10% de l'élément est visible
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
     });
 });
