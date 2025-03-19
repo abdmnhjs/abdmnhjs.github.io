7f95f2c38ad11c13890927015dee851b7540410e
abd <22403728@me.uvsq.fr>
2025-03-19 09:26:02 +0100

    qfds

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll(
-        '.technologies-title, .technologies-images img, .projets-title, .projets div .projet-title, .projets div .projet-link, .projets div .projet-description, .formations h1, .formations img, .formations p, .etudes-title, .etudes-description'
-    );
+    const elements = document.querySelectorAll('.skill, .project');
 
     let delay = 150;
     elements.forEach((element, index) => {
@@ -10,23 +8,5 @@ document.addEventListener("DOMContentLoaded", function() {
             element.style.transform = 'translateY(0)';
         }, delay);
         delay += 150;
-    });
-
-    const choixForm = document.getElementById('choixForm');
-    choixForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const choix = document.getElementById('choix').value;
-        fetch('process.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `choix=${choix}`
-        })
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('content').innerHTML = data.content;
-            })
-            .catch(error => console.error('Error:', error));
     });
 });
